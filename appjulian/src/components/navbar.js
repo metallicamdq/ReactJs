2e8f84d0f93bb4e6ba4a922aebc83589bddd59a1
julian <schreiber.julian@gmail.com>
2022-05-16 19:04:12 -0300

    version 3.0

---
+++ appjulian/src/components/navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./navbar.css"
+
+
+const Navbar =()=>{
+    return(
+        <nav className="nav">
+            <img className="logo" src="https://www.bestshoes.cl/arquivos/logo_bestshoes.png?v=637566209027400000" alt=""></img>
+            <ul className="nav-menu">
+                <li className="nav-menu_li"><a href="#">Inicio</a></li>
+                <li className="nav-menu_li"><a href="#">Productos</a></li>
+                <li className="nav-menu_li"><a href="#">Categorias</a></li>
+                <li className="nav-menu_li"><a href="#">Servicios</a></li>
+            </ul>
+            <button className="btn-interaz" ><i class="fa-solid fa-cart-shopping"></i></button>
+            <button  className="btn-interaz"><i class="fa-solid fa-user"></i></button>
+        </nav>
+    );
+}
+
+export default Navbar;
